e3b0e8d190afaf668db74412936641988b994075
Bogdan <bogdanzsoldos94@gmail.com>
2024-07-31 10:23:20 +0200

    Added DaysInFebruary class to Homework

---
+++ src/main/java/homework/javaintro/DaysInFeb.java
@@ -0,0 +1,31 @@
+package homework.javaintro;
+
+import java.util.Scanner;
+
+public class DaysInFeb {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Adauga un an intre 1900 si 2016:");
+        int year = scanner.nextInt();
+        scanner.close();
+
+        if (year < 1900 || year > 2016) {
+            System.out.println("Te rog adauga un an intre 1900 si 2016:");
+        } else {
+            int daysInFebruary = getDaysInFebruary(year);
+            System.out.println("February in " + year + " has " + daysInFebruary + " days.");
+        }
+    }
+
+    private static int getDaysInFebruary(int year) {
+        if (LeapYear(year)) {
+            return 29;
+        } else {
+            return 28;
+        }
+    }
+
+    private static boolean LeapYear(int year) {
+        return (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0);
+    }
+}
