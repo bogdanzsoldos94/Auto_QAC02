004b4e9a2b2eaa77230dbf26afbac301da759851
Bogdan <bogdanzsoldos94@gmail.com>
2024-08-31 09:51:41 +0200

    Added exceptions as course.

---
+++ src/main/java/courses/java/intro/exceptions/MyCustomExceptions.java
@@ -0,0 +1,8 @@
+package courses.java.intro.exceptions;
+
+public class MyCustomExceptions extends Exception{
+
+    public MyCustomExceptions(String message) {
+        super(message);
+    }
+}
--- src/main/java/courses/java/intro/oop3/Main.java
+++ src/main/java/courses/java/intro/oop3/Main.java
@@ -5,6 +5,9 @@ import courses.java.intro.oop2.Shape;
 import courses.java.intro.oop2.Square;
 import courses.java.intro.oop2.Triangle;
 
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class Main {
     public static void main(String[] args) {
@@ -17,5 +20,14 @@ public class Main {
               new Circle(),
 
       };
+
+      Shape[] sh2 = new Shape[5];
+      sh2[0] = new Circle(5);
+      sh2[0].setColor("White");
+      System.out.println(sh2[0].getColor());
+
+        ArrayList l = new ArrayList();
+
+
     }
 }
